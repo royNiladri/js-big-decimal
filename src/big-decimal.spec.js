67151c3b29fca3df2e3e28aeb6aae981dd693f7d
royNiladri <royNiladri@users.noreply.github.com>
2017-06-24 01:53:19 +0530

    switched to phantomJS browser for travis

--- src/big-decimal.spec.js
+++ src/big-decimal.spec.js
@@ -1,56 +1,56 @@
-describe('big-decimal', () => {
-    //let bigDecimal = require('./big-decimal');
+describe('big-decimal', function() {
+    //var bigDecimal = require('./big-decimal');
 
-    it('should be defined', () => {
+    it('should be defined', function() {
         expect(bigDecimal).toBeDefined();
     });
 
-    it('should return same value after creating Object', () => {
-        let n = new bigDecimal(12.34);
+    it('should return same value after creating Object', function() {
+        var n = new bigDecimal(12.34);
         expect(n.getValue()).toBe('12.34');
     });
 
-    it('should not take non-numeric argument', () => {
+    it('should not take non-numeric argument', function() {
         try{
-            let n = new bigDecimal('notAnumber');
+            var n = new bigDecimal('notAnumber');
             expect(true).toBe(false);
         }catch(e){
             expect(e.toString()).toMatch('Parameter is not a number');
         }
     });
 
-    it('should round off 12.678 to 12.68 if precision is set to 2', () => {
-        let n = new bigDecimal('12.678');
+    it('should round off 12.678 to 12.68 if precision is set to 2', function() {
+        var n = new bigDecimal('12.678');
         expect(n.round(2)).toBe('12.68');
     });
 
-    it('should round off 12.678 to 13 if precision is not passed', () => {
-        let n = new bigDecimal('12.678');
+    it('should round off 12.678 to 13 if precision is not passed', function() {
+        var n = new bigDecimal('12.678');
         expect(n.round()).toBe('13');
     });
 
-    it('should round off 12.2678 to 12 if precision is set to 0', () => {
-        let n = new bigDecimal('12.2678');
+    it('should round off 12.2678 to 12 if precision is set to 0', function() {
+        var n = new bigDecimal('12.2678');
         expect(n.round(0)).toBe('12');
     });
 
-    it('should round off to throw error if precision is not a number', () => {
+    it('should round off to throw error if precision is not a number', function() {
         try{
-            let n = new bigDecimal('12.2678');
-            let r = n.round('zero');
+            var n = new bigDecimal('12.2678');
+            var r = n.round('zero');
             expect(true).toBeFalsy();
         }catch(e){
             expect(e).toMatch('Precision is not a number');
         }
     });
 
-    it('should transform 1567866522.26567 to 1,567,866,522.26567 if pretty is required', () => {
-        let n = new bigDecimal('1567866522.26567');
+    it('should transform 1567866522.26567 to 1,567,866,522.26567 if pretty is required', function() {
+        var n = new bigDecimal('1567866522.26567');
         expect(n.getPrettyValue()).toBe('1,567,866,522.26567');
     });
 
-    it('should transform 1234567890123456 to 1234-5678-9012-3456 if pretty is required', () => {
-        let n = new bigDecimal('1234567890123456');
+    it('should transform 1234567890123456 to 1234-5678-9012-3456 if pretty is required', function() {
+        var n = new bigDecimal('1234567890123456');
         expect(n.getPrettyValue(4, '-')).toBe('1234-5678-9012-3456');
     });
 })
--- src/round.spec.js
+++ src/round.spec.js
@@ -1,10 +1,10 @@
-describe('round', ()=>{
+describe('round', function(){
 
-    it('should return integer unchanged', ()=>{
+    it('should return integer unchanged', function(){
         expect(roundOff(123456)).toBe('123456');
     });
     
-    it('should return float with padded zeros is second argument is non-zero and first is integer', ()=>{
+    it('should return float with padded zeros is second argument is non-zero and first is integer', function(){
         expect(roundOff(123456, 2)).toBe('123456.00');
     })
 })
