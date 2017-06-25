dc4cf3573740a5110867797bc2b276ecf25e4324
Niladri Roy <royNiladri@users.noreply.github.com>
2017-06-25 22:41:41 +0530

    Addition and substraction functionality developed. To be wired...

---
+++ src/add.spec.js
@@ -0,0 +1,41 @@
+describe('add', function(){
+    it('should be defined', function(){
+        expect(add).toBeDefined();
+    });
+
+    it('should: 12+13 = 25', function(){
+        expect(add('12', '13')).toBe('25');
+    });
+
+    it('should: 12-13 = -1', function(){
+        expect(add('12', '-13')).toBe('-1');
+    });
+
+    it('should: 12.12+13.94 = 26.06', function(){
+        expect(add('12.12', '13.94')).toBe('26.06');
+    });
+
+    it('should: 12-135 = -123', function(){
+        expect(add('12', '-135')).toBe('-123');
+    });
+
+    it('should: 12.67+13 = 25.67', function(){
+        expect(add('12.67', '13')).toBe('25.67');
+    });
+
+    it('should: -12.67+13 = 0.33', function(){
+        expect(add('-12.67', '13')).toBe('0.33');
+    });
+
+    it('should: 12.67-13 = -0.33', function(){
+        expect(add('12.67', '-13')).toBe('-0.33');
+    });
+
+    it('should: -12.67-13 = -0.33', function(){
+        expect(add('-12.67', '-13')).toBe('-25.67');
+    });
+
+    it('should: 12.67+.13 = 12.80', function(){
+        expect(add('12.67', '.13')).toBe('12.80');
+    });
+})
