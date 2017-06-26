24c33e9c457fed814b701b7914f457e0b7cc2394
Niladri Roy <royNiladri@users.noreply.github.com>
2017-06-26 15:09:35 +0530

    gulp to build and test.

--- src/round.js
+++ src/round.js
@@ -1,70 +1,74 @@
-function roundOff(input, n){
-    if(!n)
-        n = 0;
-    if(typeof(input) == 'number')
-        input += '';
+var roundOff = (function () {
+    function roundOff(input, n) {
+        if (!n)
+            n = 0;
+        if (typeof (input) == 'number')
+            input += '';
 
-    var five = '';
-    var parts = input.split('.');
-    var partInt = parts[0];
+        var five = '';
+        var parts = input.split('.');
+        var partInt = parts[0];
 
-    if(n == 0){
-        var l = partInt.length;
+        if (n == 0) {
+            var l = partInt.length;
 
-        if(greaterThanFive(parts[1], partInt)){
-            return increment(partInt);
+            if (greaterThanFive(parts[1], partInt)) {
+                return increment(partInt);
+            }
+            return partInt;
         }
-        return partInt;
-    }
 
-    if(!parts[1]){
-        return partInt+'.'+(new Array(n+1).join('0'));
-    }else if(parts[1].length < n){
-        return parseInt+'.'+(new Array(n - parts[1].length + 1).join('0'));
-    }
-
-    var partDec = parts[1].substring(0, n);
-    var rem = parts[1].substring(n);
-    
-    if(greaterThanFive(rem, partDec)){
-        partDec = increment(partDec);
-        if(partDec.length > n){
-            return increment(partInt, parseInt(partDec[0]))+'.'+partDec.substring(1);
+        if (!parts[1]) {
+            return partInt + '.' + (new Array(n + 1).join('0'));
+        } else if (parts[1].length < n) {
+            return parseInt + '.' + (new Array(n - parts[1].length + 1).join('0'));
         }
-    }
-    return partInt+'.'+partDec;
-}
 
-function greaterThanFive(part, pre){
-    if(!part)
-        return false;
-    
-    var five = '5' + (new Array(part.length+1).join('0'));
-    return (part > five || (part == '5' && parseInt(pre[pre.length - 1])%2 == 1));
-}
+        var partDec = parts[1].substring(0, n);
+        var rem = parts[1].substring(n);
 
-function increment(part, c){
-    if(!c)
-        c = 1;
-    if(typeof(part) == 'number')
-        part += '';
-
-    var l = part.length - 1;
-    var s = '';
-
-    for(var i = l; i >= 0; i--){
-        x = parseInt(part[i]) + c;
-        if(x == 10){
-            c = 1; x = 0;
-        }else{
-            c = 0;
+        if (greaterThanFive(rem, partDec)) {
+            partDec = increment(partDec);
+            if (partDec.length > n) {
+                return increment(partInt, parseInt(partDec[0])) + '.' + partDec.substring(1);
+            }
         }
-        s+=x;
+        return partInt + '.' + partDec;
     }
-    if(c)
-        s += c;
-    
-    return s.split('').reverse().join('');
-}
+
+    function greaterThanFive(part, pre) {
+        if (!part)
+            return false;
+
+        var five = '5' + (new Array(part.length + 1).join('0'));
+        return (part > five || (part == '5' && parseInt(pre[pre.length - 1]) % 2 == 1));
+    }
+
+    function increment(part, c) {
+        if (!c)
+            c = 1;
+        if (typeof (part) == 'number')
+            part += '';
+
+        var l = part.length - 1;
+        var s = '';
+
+        for (var i = l; i >= 0; i--) {
+            x = parseInt(part[i]) + c;
+            if (x == 10) {
+                c = 1; x = 0;
+            } else {
+                c = 0;
+            }
+            s += x;
+        }
+        if (c)
+            s += c;
+
+        return s.split('').reverse().join('');
+    }
+
+    return roundOff;
+})();
 
 //module.exports = roundOff;
