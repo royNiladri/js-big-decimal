c0602fb01a61af82187ca1bf69bf4c3acea47d51
royNiladri <royNiladri@users.noreply.github.com>
2017-07-11 00:19:28 +0530

    Division fixed

--- src/divide.spec.js
+++ src/divide.spec.js
@@ -2,15 +2,17 @@ var divide = require('../lib/divide').divide;
 
 describe('divide', function(){
     it('should return correct result', function(){
-        let count = 100, err = 0;
+        let count = 1000, err = 0;
         while(count-- > 0){
-            let dividend = Math.round(Math.random()*(Math.pow(10, Math.floor(Math.random()*5))));
-            let divisor = Math.round(Math.random()*(Math.pow(10, Math.floor(Math.random()*5))));
-            if(divisor==0)
+            let dividend = (Math.random()*(Math.pow(10, Math.floor(Math.random()*10)))).toFixed(5);
+            let divisor = (Math.random()*(Math.pow(10, Math.floor(Math.random()*10))));
+            if(divisor==0){
+                count++;
                 continue;
+            }
             let jsOut = dividend/divisor;
-            let myOut = parseFloat(divide(dividend, divisor));
-            if(Math.abs(jsOut-myOut)>0.001){
+            let myOut = parseFloat(divide(dividend, divisor, 5));
+            if(isNaN(myOut) || Math.abs(jsOut-myOut)>0.00001){
                 //console.log(`${dividend}/${divisor} = ${jsOut} != ${myOut}`);
                 err++;
             }else
@@ -22,5 +24,11 @@ describe('divide', function(){
     });
     it('should do basic integer division', function(){
         expect(divide(1234, 12, 2)).toBe('102.83');
+    })
+    it('should do basic floating point division', function(){
+        expect(divide(12.34, 12, 2)).toBe('1.03');
+    })
+    it('should do basic floating point division', function(){
+        expect(divide(29629629362962961839.48344234, 12345678901234567890, 2)).toBe('2.40');
     })
 })
--- src/divide.ts
+++ src/divide.ts
@@ -1,4 +1,6 @@
-import { add } from './add'
+import { add, trim } from './add';
+import { roundOff } from "./round";
+
 export function divide(dividend, divisor, precission=8){
     if(divisor == 0){
         throw new Error('Cannot divide by 0');
@@ -7,15 +9,42 @@ export function divide(dividend, divisor, precission=8){
     dividend = dividend.toString();
     divisor = divisor.toString();
 
-    var pt_loc = (dividend.indexOf('.')>0?dividend.indexOf('.'):0)-(divisor.indexOf('.')>0?divisor.indexOf('.'):0);
-
-    dividend = dividend.replace('.', '');
-    divisor = divisor.replace('.', '');
-
-    let prec = 0, dl = divisor.length, rem = '0', quotent = '', maxIter = 10;
+    //var pt_loc = (dividend.indexOf('.')>0?dividend.length-dividend.indexOf('.')-1:0)-(divisor.indexOf('.')>0?divisor.length-divisor.indexOf('.')-1:0);
+    var pt_dvsr = divisor.indexOf('.')>0?divisor.length-divisor.indexOf('.')-1:-1;
+    
+    divisor = trim(divisor.replace('.', ''));
+    if(pt_dvsr>=0){
+        let pt_dvnd = dividend.indexOf('.')>0?dividend.length-dividend.indexOf('.')-1:-1;
+        
+        //if dividend has no decimal pt
+        if(pt_dvnd == -1){
+            dividend = trim(dividend+(new Array(pt_dvsr+1)).join('0'));
+        }else{
+            //if pt_dvsr>pt_dvnd -> 12.45/10.106 or 12.2/1.47 -> shift right
+            if(pt_dvsr>pt_dvnd){
+                dividend = dividend.replace('.', '');
+                dividend = trim(dividend+(new Array(pt_dvsr-pt_dvnd+1)).join('0'));
+            }
+            //if pt_dvsr<pt_dvnd -> 12.40/14.1 or 0.078/0.7 -> shift left
+            else if(pt_dvsr<pt_dvnd){
+                dividend = dividend.replace('.', '');
+                let loc = dividend.length-pt_dvsr+pt_dvsr;
+                dividend = trim(dividend.substring(0, loc)+'.'+dividend.substring(loc));
+            }
+            //if equal
+            else if(pt_dvsr==pt_dvnd){
+                dividend = trim(dividend.replace('.', ''));
+            }
+        }
+    }
+    //console.log('equivalent:'+dividend+'/'+divisor);
+    let prec = 0, dl = divisor.length, rem = '0', quotent = '';
     let dvnd = dividend.substring(0, dl);
     dividend = dividend.substring(dl);
-    while(prec<=precission && --maxIter){
+
+    precission = precission+2;//-pt_loc;
+
+    while(prec<=precission){
         //console.log(dvnd+', '+dividend);
         let qt = 0;
         while(parseInt(dvnd)>=parseInt(divisor)){
@@ -32,9 +61,22 @@ export function divide(dividend, divisor, precission=8){
             prec++;
             dvnd = dvnd+'0';
         }else{
+            if(dividend[0]=='.'){
+                quotent+='.';
+                prec++;
+                dividend = dividend.substring(1);
+            }
             dvnd = dvnd + dividend.substring(0,1);
             dividend = dividend.substring(1);
         }
     }
-    return quotent;
+    //console.log(quotent);
+    /*if(pt_loc>=0){
+        let new_loc = (quotent.indexOf('.')?quotent.indexOf('.'):quotent.length)-pt_loc;
+        quotent = quotent.replace('.', '');
+        quotent = quotent.substring(0, new_loc)+'.'+quotent.substring(new_loc);
+        precission = precission + pt_loc;
+    }*/
+
+    return roundOff(quotent, precission-2);
 }
