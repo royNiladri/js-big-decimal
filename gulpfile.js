c6062cde8b090b4a281c5f5828ca47f05a130b5d
royNiladri <royNiladri@users.noreply.github.com>
2017-07-16 03:07:53 +0530

    bundling for distribution
    Added webpack and gulp in the mix. usage will change.
    var bigDecimal = require('js-big-decimal'); // JS
    import bigDecimal = require('js-big-decimal'); // TS

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var del = require('del');
+var run = require('gulp-run');
+
+const paths = {
+    src: ['./src/**/*.ts'],
+    entry: './src/big-decimal.ts',
+    lib: './lib/',
+    dist: './dist/'
+}
+
+const webpackConfig = require('./webpack.config.js');
+
+gulp.task('clean', function () {
+    return del(['./dist/*']);
+})
+
+gulp.task('dist', ['clean'], function () {
+
+    var cmd = new run.Command('webpack', { usePowerShell: true, silent: true });
+
+    //web non-prod
+    process.env.USE_ENV = 'web';
+    process.env.NODE_ENV = 'dev';
+    cmd.exec();
+
+    //web prod
+    process.env.USE_ENV = 'web';
+    process.env.NODE_ENV = 'production';
+    cmd.exec();
+
+    //node non-prod
+    process.env.USE_ENV = 'node';
+    process.env.NODE_ENV = 'dev';
+    cmd.exec();
+
+    //node prod
+    process.env.USE_ENV = 'node';
+    process.env.NODE_ENV = 'production';
+    cmd.exec();
+
+    gulp.src([`${paths.lib}/big-decimal.d.ts`])
+        .pipe(gulp.dest(`${paths.dist}/node`));
+});
