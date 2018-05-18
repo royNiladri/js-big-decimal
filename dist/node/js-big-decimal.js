2121c9893d26bc692b13fc6c5c5f10d212a7a2b7
royNiladri <royNiladri@users.noreply.github.com>
2018-05-19 00:45:50 +0530

    dependecies updated to latest

--- dist/node/js-big-decimal.js
+++ dist/node/js-big-decimal.js
@@ -7,7 +7,7 @@
 		exports["bigDecimal"] = factory();
 	else
 		root["bigDecimal"] = factory();
-})(this, function() {
+})(typeof self !== 'undefined' ? self : this, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
