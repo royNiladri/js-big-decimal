daaaaea23c2c41570e662d48d89857901eb84f91
Jsoto22 <sotojared22@gmail.com>
2024-12-13 21:03:54 -0500

    Added dedicated exponential calculation and logarithms testing

--- src/logarithm.ts
+++ src/logarithm.ts
@@ -1,10 +1,10 @@
 import { abs } from "./abs";
 import { add } from "./add";
-import { lessThan, equals, greaterThan } from "./compareTo";
+import { equals, greaterThan, isExatclyOne, isExatclyZero } from "./compareTo";
 import { E, LN10, LN2 } from "./constants";
 import { divide } from "./divide";
 import { multiply } from "./multiply";
-import { intPow, pow } from "./pow";
+import { intPow } from "./pow";
 import { roundOff } from "./round";
 import { stripTrailingZero } from "./stripTrailingZero";
 import { subtract } from "./subtract";
@@ -27,8 +27,29 @@ export function Euler(precision: number = 64) {
     }
 }
 
-export function exp(exponent: string) {
-    return pow(E, exponent, 64);
+export function exp(exponent: string, precision: number = 32) {
+    exponent = stripTrailingZero(exponent);
+
+    if(isExatclyZero(exponent)) return '1';
+
+    if(!exponent.includes('.')){
+        let intExp = intPow(E, exponent);
+        if(exponent[0] == '-') intExp = divide('1', intExp, precision);
+        return stripTrailingZero(roundOff(intExp, precision));
+    }
+
+    let result = '1';
+    let n = '1';
+    let f = '1';
+    while (true) {
+        f = multiply(f, n);
+        const next = stripTrailingZero(divide(intPow(exponent, n), f, precision + parseInt(n)))
+        if (testTolerance(abs(next), precision + parseInt(n))) {
+            return stripTrailingZero(roundOff(add(result, next), precision));
+        }
+        result = add(result, next);
+        n = add(n, '1');
+    }
 }
 
 export function expm1(exponent: string) {
@@ -63,18 +84,25 @@ export function ln(x: string = '2') {
 export function ln2(x: string = '2') {
     validateGTZero(x, 'ln2');
 
+    if (isExatclyOne(x)) {
+        return '0'; // ln(1) = 0
+    }
+
     let result = '0';
     while (greaterThan(x, '2', true)) {
         x = stripTrailingZero(divide(x, '2', 68));
         result = add(result, '1');
     }
-    return roundOff(add(result, divide(ln(x), LN2, 68)), 64);
+
+    if(isExatclyOne(x)) return result;
+
+    return stripTrailingZero(roundOff(add(result, divide(ln(x), LN2, 68)), 64));
 }
 
 export function log(base: string) {
-    return roundOff(multiply(ln2(base), LN2), 64);
+    return ln(base);
 }
 
 export function log10(base: string) {
-    return roundOff(divide(ln(base), LN10, 64 + 2), 64);
+    return roundOff(divide(ln(base), LN10, 68), 64);
 }
